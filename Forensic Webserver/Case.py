0d817019709ef9dd63f5bce68e528547aad7e7ae
MichaelBotha-Repos <mbotha88@gmail.com>
2022-01-17 10:39:49 +0200

    Flask routes + HTML files
    Create most of the Flask routes, and HTML files

---
+++ Forensic Webserver/Case.py
@@ -0,0 +1,6 @@
+"""
+    This module sets the logic required for operations related
+    to cases, such as ...
+
+"""
+
--- Forensic Webserver/application.py
+++ Forensic Webserver/application.py
@@ -1,4 +1,4 @@
-""" This is a the main module for the Flask webserver """
+""" This is the main module for the Flask webserver """
 
 from flask import Flask, render_template, request, redirect
 import json
@@ -22,6 +22,11 @@ def json_io(login_dict):
 @app.route("/", methods = ["GET", "POST"])
 @app.route("/home", methods = ["GET", "POST"])
 def homepage():
+    """
+        The home page route function displays the login page, requests log in,
+        and processes the login details provided
+    """
+    
     msg = request.method
     if msg == "GET":
         return render_template("index.html")
@@ -30,17 +35,86 @@ def homepage():
         name = request.form.get("name")
         password = request.form.get("password")
         login = {name : password}
+        # Might need to make json_io a class and run the rest of
+        # this routes code in the thread so that the main thread
+        # can revert back to the normal webserver listening 
+        
         auth_thread = Thread(target= json_io, args= (login,) )
         auth_thread.start()
         # Send JSON object to Authentication module
-        return "successful"
+        return render_template("/options.html")
         
         
         # Process returned information
         # if(returned value)== True:
         # redirect to options page
         # else:
-        # inform user 
+        # inform user
+
+# Options page logic
+@app.route("/options", methods = ["GET", "POST"])
+def options():
+    """
+        The options page provides an initial means to the user
+        to navigate to the required service.
+    """
+    
+    # check authentication possibly by a session cookie
+    # if authenticated then proceed
+    msg = request.method
+    if msg == "GET":
+        return render.template("options.html")
+
+    elif (msg == "POST") and # logout selected 
+
+
+# Search page logic
+@app.route("/search", methods = ["GET", "POST"])
+def search():
+    """
+        The search page allows a user to enter a case number,
+        creation date, or/and name substring to reveal the cases 
+        whose values contain the entered ones.       
+    """
+
+# Edit page logic
+@app.route("/edit", methods = ["GET", "POST"])
+def edit():
+    """
+        The edit case page requires a user to enter a case
+        number for a case to edit then redirects to the
+        specific case.
+    """
+
+# Create page logic 
+@app.route("/create", methods = ["GET", "POST"])
+def create():
+    """
+        The create page allows a user to create a case
+        by first entering its related metadata, after
+        which the page is redirected to the edit page.
+    """
+
+
+# Delete page logic 
+@app.route("/delete", methods = ["GET", "POST"])
+def create():
+    """
+        The delete page allows a user to select and
+        delete a case.
+    """
+
+
+
+
+
+
+
+
+
+
+
+
         
 
 app.run()
